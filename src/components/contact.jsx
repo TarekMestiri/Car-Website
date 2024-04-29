7d506a231ad8bab60dd38e1e11af412f135cff3a
TarekMestiri <tarek.jlassi.monastiri@gmail.com>
2024-04-29 22:28:32 +0100

    hello

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -126,26 +126,7 @@ export const Contact = (props) => {
             </div>
           </div>
           <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
+            <div className="row">                              
             </div>
           </div>
         </div>
@@ -153,10 +134,7 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
+            &copy; 2024 Tarek's first website             
           </p>
         </div>
       </div>
